a6a14163821c59da1161ec9572b76e5079b5216e
Matt Heerspink <mheerspink75@gmail.com>
2021-07-18 14:58:15 -0500

    moved chart to new file chart.js

---
+++ src/chart.js
@@ -0,0 +1,44 @@
+// Fetch url
+const fetchData = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+    if (res.status !== 200) {
+      throw new Error();
+    }
+    chart(data);
+  };
+  
+  //Prepare data and render the chart
+  const chart = (data) => {
+    let arr = [];
+    // Prepare Data
+    for (const key of data.Data.Data) {
+      let data = [key.time * 1000, key.close];
+      arr.push(data);
+    }
+    // Create the line chart
+    Highcharts.stockChart("container", {
+      chart: {
+        backgroundColor: "white",
+        type: "line",
+      },
+  
+      title: {
+        text: `<h1 id="chart-title">${symbol.value}</h1>`,
+        align: "left",
+      },
+  
+      series: [
+        {
+          name: symbol.value,
+          data: arr,
+          tooltip: {
+            valueDecimals: 2,
+          },
+        },
+      ],
+    });
+  };
+  
+  export default fetchData;
+  
