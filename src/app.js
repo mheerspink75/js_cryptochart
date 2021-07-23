b3db4fe69b4e24406f6b8f0adba2d7378584585a
Matt Heerspink <mheerspink75@gmail.com>
2021-07-23 11:36:40 -0500

    removed cc.js and updated functions app.js/chart.js

--- src/app.js
+++ src/app.js
@@ -14,20 +14,39 @@ const request = async () => {
   let data = await response.json();
 
   let key = [];
+  let key1 = [];
+  let key2 = [];
+  let key3 = [];
 
-  // Push symbols to key array
+  // Push symbols, fullName and imageurl to arrays
   for (let n of data.Data) {
     let coinName = n.CoinInfo.Name;
+    let fullName = n.CoinInfo.FullName;
+    let imageUrl = n.DISPLAY.USD.IMAGEURL;
+    let price = n.DISPLAY.USD.PRICE;
     key.push(coinName);
+    key1.push(fullName);
+    key2.push(imageUrl);
+    key3.push(price);
   }
+
+  // console.log(key, key1, key2, key3)
 
   // Default key[0] onload
   if (btn.event === "click") {
     const sb = document.querySelector("#symbol");
     var symbol = key[sb.selectedIndex];
+    var fullName = key1[sb.selectedIndex];
+    var img = "https://www.cryptocompare.com" + key2[sb.selectedIndex];
+    var price = key3[sb.selectedIndex];
   } else {
     var symbol = key[0];
+    var fullName = key1[0];
+    var img = "https://www.cryptocompare.com" + key2[0];
+    var price = key3[0]
   }
+
+
 
   // Populate select options
   for (let index of key) {
@@ -51,6 +70,12 @@ const request = async () => {
     console.log("Error!", err.message);
     document.getElementById("app").innerHTML = "Error!: " + err.message;
   });
+
+  document.getElementById("price").innerHTML = price;
+  document.getElementById("fullName").innerHTML = fullName;
+  document.querySelector("img").alt = fullName;
+  document.querySelector("img").src = img;
+  console.log(symbol + price)
 };
 
 // Button
--- src/chart.js
+++ src/chart.js
@@ -1,6 +1,3 @@
-import { all_the_coins } from "./cc.js";
-
-//console.log(all_the_coins)
 
 // Fetch url
 const fetchData = async (url) => {
@@ -9,16 +6,11 @@ const fetchData = async (url) => {
   if (res.status !== 200 || !res.ok) {
     throw new Error();
   }
-  let fullName = all_the_coins.Data[symbol.value].CoinName;
-  let description = all_the_coins.Data[symbol.value].Description;
-
-  document.getElementById("fullName").innerHTML = fullName;
-  document.getElementById("description").innerHTML = description;
   return chart(data);
 };
-
 //Prepare data and render the chart
 const chart = (data) => {
+
   let arr = [];
   // Prepare Data
   for (const key of data.Data.Data) {
@@ -26,61 +18,42 @@ const chart = (data) => {
     arr.push(data);
   }
 
-  // Get current price and image
-  let priceUrl =
-    "https://min-api.cryptocompare.com/data/pricemultifull?fsyms=" +
-    `${symbol.value}` +
-    "&tsyms=USD";
-
-  fetch(priceUrl)
-    .then((response) => response.json())
-    .then((data) => {
-      let price = data.DISPLAY[symbol.value].USD.PRICE;
-      let img =
-        "https://www.cryptocompare.com" +
-        data.DISPLAY[symbol.value].USD.IMAGEURL;
-
-      document.querySelector("img").src = img;
-      document.querySelector("img").alt = fullName;
-
-      // Create the line chart
-      Highcharts.stockChart("container", {
-        chart: {
-          backgroundColor: "white",
-          type: "area",
+  // Create the line chart
+  Highcharts.stockChart("container", {
+    chart: {
+      backgroundColor: "white",
+      type: "area",
+    },
+    title: {
+      text: `<h1 id="chart-title">${symbol.value}/USD</h1>`,
+      align: "left",
+    },
+    plotOptions: {
+      series: {
+        fillColor: {
+          linearGradient: [0, 0, 0, 300],
+          stops: [
+            [0, Highcharts.getOptions().colors[0]],
+            [
+              1,
+              Highcharts.color(Highcharts.getOptions().colors[0])
+                .setOpacity(0)
+                .get("rgba"),
+            ],
+          ],
         },
-        title: {
-          text: `<h1 id="chart-title">${symbol.value}/USD  ${price}</h1>`,
-          align: "left",
+      },
+    },
+    series: [
+      {
+        name: symbol.value,
+        data: arr,
+        tooltip: {
+          valueDecimals: 2,
         },
-        plotOptions: {
-          series: {
-            fillColor: {
-              linearGradient: [0, 0, 0, 300],
-              stops: [
-                [0, Highcharts.getOptions().colors[0]],
-                [
-                  1,
-                  Highcharts.color(Highcharts.getOptions().colors[0])
-                    .setOpacity(0)
-                    .get("rgba"),
-                ],
-              ],
-            },
-          },
-        },
-        series: [
-          {
-            name: symbol.value,
-            data: arr,
-            tooltip: {
-              valueDecimals: 2,
-            },
-          },
-        ],
-      });
-    })
-    .catch((err) => console.log(err.message));
+      },
+    ],
+  })
 };
 
 export default fetchData;
